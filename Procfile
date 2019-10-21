1744ee869e26a6fa9041afc4cd09ca038b0ddbee
vcable <skeblikk@gmail.com>
2019-10-21 15:16:17 -0700

    Procfile/server changes

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python main.py
+web: python server.py
--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from os import environ
 from flask import Flask
 
 # Server for Heroku
-#app = Flask(__carti__)
-app.run(environ.get('PORT'))
+app = Flask(__name__)
+app.run(host= "0.0.0.0", port=environ.get("PORT"))
